fbd0ff743819f3126b7746801de03aec16b375bf
Nathan-Zhao0806 <zhaon2273@my.sduhsd.net>
2026-03-11 03:06:10 +0000

    something2

--- src/nav.jsx
+++ src/nav.jsx
@@ -6,15 +6,9 @@ function Nav(){
     return (
         <div className='navibar'>
             <ul>
-<<<<<<< HEAD
-                <li>Home</li>
-                <li href='./main2.jsxs'>About</li>
-                <li>Contact</li>
-=======
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/about">About</Link></li>
                 <li><Link to="/contact">Contact</Link></li>
->>>>>>> 25da354 (add the nav bar)
             </ul>
         </div>
     )
