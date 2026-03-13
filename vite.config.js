650baa123976288067c0628b48bb137924f74473
Nathan-Zhao0806 <zhaon2273@my.sduhsd.net>
2026-03-13 13:30:02 -0700

    fixxx

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-app-Nathan-Zhao0806/',
+  base: '/',
 });
 
