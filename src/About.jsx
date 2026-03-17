aad1aef00ae802c9eff9b1440bd951e52a560aaa
Nathan-Zhao0806 <zhaon2273@my.sduhsd.net>
2026-03-17 12:38:40 -0700

    about api

--- src/About.jsx
+++ src/About.jsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Nav from './nav';
 import Card from './Card';
 import './index.css'
-
+const {apiData, setapiData} = useState('');
+const fetchDogImage = async () => {
+    try {
+      const response = await fetch('https://dog.ceo/api/breeds/image/random');
+      const data = await response.json();
+      setapiData(data.message); // "message" is the field of the image URL
+    } catch (error) {
+      console.error('Error fetching dog image:', error);
+    }
+  };
+  
 function About(){
     return (
     <>
     <p>This is my page, I am Nathan Z</p>
     <Card  name="Myself!" description="Add me into your cart!"/>
+    <button onClick={fetchDogImage}>Fetch Dog Image</button>
+    {apiData && <img src={apiData} alt="Random Dog" />}
+
     <footer><p>
         &copy; 2026 Nathan website</p></footer>
     </>);
